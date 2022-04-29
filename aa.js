fce79af1d304cc796cef5aff3140ad222bf296ce
archana <archana20@navgurukul.org>
2022-04-29 10:51:49 +0530

    first commit

---
+++ aa.js
@@ -0,0 +1,4 @@
+// console.log("my love");
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<h1>Hello, world!</h1>);
---
+++ reverse.js
@@ -0,0 +1,7 @@
+let x=178;
+let a=x%10;
+let b=math.floor(x/10);
+let c=b%10;
+let d=math.floor(b/10);
+console.log(a,c,d);
+
